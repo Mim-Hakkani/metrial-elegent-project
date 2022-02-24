4a1f50e0bb6bac3adcae8729786846df8226ca7f
32-Mimhakkani <mim.cse32@gmail.com>
2022-02-24 13:08:05 +0600

    about section is ready

--- src/Components/Aboutme/About.js
+++ src/Components/Aboutme/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import '../Contact/Contact.css'
 import about from '../../Assets/about.jpg'
 const About = () => {
     return (
-        <div>
+        <div style={{ marginTop:'30px',marginBottom:'50px'}}>
             <Container>
                 <Grid container spacing={2}>
                     <Grid item lg={12} md={12} sm={12}>
@@ -39,12 +39,45 @@ const About = () => {
             <Grid container spacing={2}>
 
                 <Grid item xs={12} md={6} lg={6}>
-                    <Box>
-                        
+                    <Box sx={{textAlign:'justify'}}>
+                         <Typography
+                            variant="h5" 
+                            gutterBottom 
+                            component="div">
+                            You can conquer the world.
+                         </Typography>
+
+                         <Typography
+                            variant="body2" 
+                            gutterBottom 
+                            component="div"
+                            sx={{color:'#ffaf53'}}
+                            >
+                           If you have right set of mind and thus you can rule the world. Cheers!
+                         
+                         </Typography>
+
+                         <Typography
+                            variant="body2" 
+                            gutterBottom 
+                            component="p"
+                            sx={{
+                                color:' #99998f',
+                                lineHeight: '27px',
+                                padding: "10px 15px 10px 0px"
+                            }}
+                            
+                            >
+                           Nam non erat feugiat, pellentesque mi ut, finibus tellus. Ut eget ex sodales, feugiat elit non, semper ex. Sed laoreet lectus nisl, sit amet porttitor lacus porta nec. Quisque sit amet metus sagittis, tincidunt tellus in, efficitur lorem. Sed scelerisque tellus nisl, eget tincidunt elit aliquet vitae. Integer id felis ut ipsum tristique iaculis. Mauris sed nibh enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sagittis rhoncus sollicitudin. Maecenas eget dui a neque rutrum tincidunt sed non ipsum.
+                         
+                         </Typography>
+                         <Button variant="contained" sx={{backgroundColor:'#ffaf53'}}>
+                             Learn More
+                         </Button>
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={6} lg={6}>
-                    <img src={about} alt='' />
+                    <img src={about} alt='' height={350} />
                 </Grid>
 
              
